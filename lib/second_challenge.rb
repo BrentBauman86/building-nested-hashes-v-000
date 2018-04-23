f9c631d8101e33f981412b476acce41a232493f6
Brent Bauman <brentbauman86@gmail.com>
2018-04-23 18:07:20 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch => {
+        :name => "Lord Montague"
+        :age => "53"
+      }
+      },
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+      :patriarch => {
+        :name => "Lord Capulet"
+        :age => "50"
+      }
+      }
 end
